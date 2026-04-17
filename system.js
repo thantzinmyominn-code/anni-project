3cc56edae1346c97da9d6701528fb7a1d53247e8
Thant Zin Myo Minn <thantzinmyominn@gmail.com>
2026-04-18 03:15:29 +0700

    Add files via upload

---
+++ system.js
@@ -0,0 +1,15 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+  slides.forEach(slide => slide.classList.remove("active"));
+  slides[index].classList.add("active");
+}
+
+function nextSlide() {
+  currentSlide++;
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+  showSlide(currentSlide);
+} 
